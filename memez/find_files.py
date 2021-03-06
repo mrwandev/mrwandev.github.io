0994dcce20c7de4019df28406d020101fda627db
mrwan <71457660+mrwandev@users.noreply.github.com>
2021-03-06 15:48:31 +0000

    Update

--- memez/find_files.py
+++ memez/find_files.py
@@ -1,16 +1,16 @@
 import os
 
 dir_ = input()
-extension = input()
+# extension = input()
 
 dir_path = os.path.dirname(os.path.realpath(dir_)) 
 
-# textFile = open("text.txt", "w")
+textFile = open("text.txt", "w")
 for root, dirs, files in os.walk(dir_path): 
     for file in files:
-        if file.endswith(extension+extension):
-            # textFile.write('<img class="meme" src="' + os.path.join(file) + '">\n')
-            # print('<img class="meme" src="' + os.path.join(file) + '">')
-            print(os.path.join(file))
+        # if file.endswith(".jpg") or file.endswith(".JPG") or file.endswith(".png") or file.endswith(".PNG"):
+        textFile.write('<img class="meme" src="' + os.path.join(file) + '">\n')
+        print('<img class="meme" src="' + os.path.join(file) + '">')
+            # print(os.path.join(file))
 
 pause = input()
