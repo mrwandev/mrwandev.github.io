b70653c48e741479b1738dd6b076d7a41033af89
mrwan <71457660+mrwandev@users.noreply.github.com>
2021-04-02 15:56:46 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -38,8 +38,8 @@ function redirectWithTimer(_time, direction, directionStr) {
         
     if(timeLeft == 0)
     {
-        const nextURL = 'https://my-website.com/page_b';
-        const nextTitle = 'My new page title';
+        const nextURL = direction;
+        const nextTitle = 'nop';
         const nextState = { additionalInformation: 'Updated the URL with JS' };
 
         // This will create a new entry in the browser's history, without reloading
